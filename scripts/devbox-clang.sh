900bfb7c6c724d74ee2b1e6b505006d851acd0c0
Laaaain <laaaain@0x00.network>
2026-05-15 12:45:47 -0500

    fix(ci): fix package install script

--- scripts/devbox-clang.sh
+++ scripts/devbox-clang.sh
@@ -5,5 +5,5 @@
 
 # Update the container, enable extra repos and install packages
 dnf update -y
-dnf copr enable dejan/lazygit
-grep -v '^#' ./devbox-clang.packages | xargs dnf install -y
+dnf copr enable dejan/lazygit -y
+grep -v '^#' ./devbox-clang.packages | xargs dnf install -y --skip-unavailable
