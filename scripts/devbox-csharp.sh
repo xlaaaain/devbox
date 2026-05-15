1cae6b8105ee389cc6813d90188de7fc5bfdbfc3
Laaaain <laaaain@0x00.network>
2026-05-15 14:36:54 -0500

    feat: add csharp devbox
    Added necessary files for the csharp devbox

---
+++ scripts/devbox-csharp.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Symlink distrobox shims
+./distrobox-shims.sh
+
+# Update the container, enable extra repos and install packages
+dnf update -y
+dnf copr enable dejan/lazygit -y
+dnf group install development-tools -y
+grep -v '^#' ./devbox-csharp.packages | xargs dnf install -y --skip-unavailable
