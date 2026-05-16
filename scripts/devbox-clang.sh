a2a3913cdc12a8314f41b4ec0d7b9135cdc3f27b
Laaaain <laaaain@0x00.network>
2026-05-16 10:07:41 -0500

    feat: add github cli

--- scripts/devbox-clang.sh
+++ scripts/devbox-clang.sh
@@ -7,5 +7,8 @@
 dnf update -y
 dnf copr enable dejan/lazygit -y
 dnf group install development-tools -y
+dnf install dnf5-plugins -y
+dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo -y
+dnf install gh --repo gh-cli -y
 grep -v '^#' ./devbox-default.packages | xargs dnf install -y --skip-unavailable
 grep -v '^#' ./devbox-clang.packages | xargs dnf install -y --skip-unavailable
--- scripts/devbox-rust.sh
+++ scripts/devbox-rust.sh
@@ -8,5 +8,8 @@ dnf update -y
 dnf copr enable dejan/lazygit -y
 dnf group install development-tools -y
 dnf group install c-development -y
+dnf install dnf5-plugins -y
+dnf config-manager addrepo --from-repofile=https://cli.github.com/packages/rpm/gh-cli.repo -y
+dnf install gh --repo gh-cli -y
 grep -v '^#' ./devbox-default.packages | xargs dnf install -y --skip-unavailable
 grep -v '^#' ./devbox-rust.packages | xargs dnf install -y --skip-unavailable
